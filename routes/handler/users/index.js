f4d5284e5793aba589fbebeeae49d730ca0a9357
rivaiarvian <rivai.arviannn@gmail.com>
2022-04-02 17:10:17 +0700

    [service-user] integrasi api login & JWT

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -1,5 +1,12 @@
 const apiAdapter = require("../../apiAdapter");
-const { URL_SERVICE_USER } = process.env;
+const jwt = require("jsonwebtoken");
+const {
+  URL_SERVICE_USER,
+  JWT_SERCRET,
+  JWT_SERCRET_REFRESH_TOKEN,
+  JWT_ACCESS_TOKEN_EXPIRED,
+  JWT_REFRESH_TOKEN_EXPIRED,
+} = process.env;
 
 const api = apiAdapter(URL_SERVICE_USER);
 
@@ -7,9 +14,42 @@ module.exports = {
   register: async (req, res) => {
     try {
       const user = await api.post("/users/register", req.body);
-      console.log(user);
+
       return res.status(200).json({
         data: user.data,
+      });
+    } catch (error) {
+      if (error.code === "ECONNREFUSED") {
+        return res
+          .status(500)
+          .json({ status: "error", message: "Service unavailable" });
+      }
+      const { status, data } = error.response;
+      return res.status(status).json({
+        data,
+      });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const user = await api.post("/users/login", req.body);
+      const data = user.data.data;
+
+      const token = jwt.sign({ data: data }, JWT_SERCRET, {
+        expiresIn: JWT_ACCESS_TOKEN_EXPIRED,
+      });
+      const refreshToken = jwt.sign({ data: data }, JWT_SERCRET_REFRESH_TOKEN, {
+        expiresIn: JWT_REFRESH_TOKEN_EXPIRED,
+      });
+
+      await api.post("/refresh_token", {
+        refreshToken: refreshToken,
+        userId: data.id,
+      });
+
+      return res.status(200).json({
+        status: "success",
+        data: { token, refreshToken: refreshToken },
       });
     } catch (error) {
       if (error.code === "ECONNREFUSED") {
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { register } = require("./handler/users");
+const { register, login } = require("./handler/users");
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
