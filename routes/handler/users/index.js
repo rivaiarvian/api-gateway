1ec2310abfcf47f79bfab5e5894206a6e889b87b
rivaiarvian <rivai.arviannn@gmail.com>
2022-04-03 09:38:36 +0700

    integrasi api update user

--- routes/handler/users/index.js
+++ routes/handler/users/index.js
@@ -62,4 +62,22 @@ module.exports = {
       });
     }
   },
+  update: async (req, res) => {
+    try {
+      // return res.status(200).json(req.user);
+      const id = req.user.data.id;
+      const user = await api.put(`/users/${id}`, req.body);
+      return res.status(200).json(user.data);
+    } catch (error) {
+      if (error.code === "ECONNREFUSED") {
+        return res
+          .status(500)
+          .json({ status: "error", message: "Service unavailable" });
+      }
+      const { status, data } = error.response;
+      return res.status(status).json({
+        data,
+      });
+    }
+  },
 };
